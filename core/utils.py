61336aa5adbdfdfbc7626abc99e2c51d0721778e
DaynakOff <aleksandr.daynakov@mail.ru>
2025-05-18 15:43:19 +0700

    Добавлена логика работы с БД

---
+++ core/utils.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from .models import Users, Coords, PerewalAdd, Image
+
+
+class DataManager:
+	def __init__(self):
+		pass
+
+
+	def save_perewal(self, data):
+
+		# создаем координаты
+		coords = Coords.objects.create(
+			latitude=data['latitude'],
+			longitude=data['longitude'],
+			height=data['height'])
+
+		# создаем перевал
+		perewal = PerewalAdd.objects.create(
+			title=data['title'],
+			beauty_title=data['beauty_title'],
+			other_title=data['other_title'],
+			connect=data['connect'],
+			add_time=data['add_time'],
+			summer_level=data['summer_level'],
+			winter_level=data['winter_level'],
+			autumn_level=data['autumn_level'],
+			spring_level=data['spring_level'],
+			coords=coords)
+
+		# добавляем изображения
+		for img in data.get('images', []):
+			Image.objects.create(
+				image=img,
+				perewal_added=perewal)
+
+		return perewal.id
